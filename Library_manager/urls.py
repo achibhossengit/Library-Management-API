92b21d3bb68e4d30e8ae047f56b86551e404cfa9
achibhossengit <mail.achibhossen@gmail.com>
2025-04-16 11:22:06 +0600

    add view and url for redirect from home url to api-root url

--- Library_manager/urls.py
+++ Library_manager/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from Library_manager.views import api_root_view
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -22,7 +22,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
+    path('', api_root_view),
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
+    path('api/v1/', include('api.urls'), name='api-root'),
     path('api-auth/', include('rest_framework.urls')),
 ]
